54085c85e4cad61c55ffbd642a0175652741026e
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-27 12:44:57 -0400

    Done1

--- src/main.java
+++ src/main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class main {
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    public static boolean hasTeen(int num1, int num2, int num3){
+        if(num1 >= 12 && num1 <= 19){
+            return true;
+        } else if(num2 >= 12 && num2 <= 19){
+            return true;
+        } else return num3 >= 12 && num3 <= 19;
+    }
 }
